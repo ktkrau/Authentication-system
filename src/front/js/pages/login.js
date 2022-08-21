7c81dd231feede26b44b23e2637ee166910039c8
ktkrau <ktkrau@gmail.com>
2022-08-21 05:05:05 +0000

    l

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,48 +1,40 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	const token = sessionStorage.getItem("token");
+	console.log("This is your token", store.token)
 
 	const handleClick = () => {
+		actions.login(email, password)/* .then(() => {
+			history.push("/")
 
-		const opts = {
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				"email": email,
-				"password": password
-			})
-		}
-		fetch('https://3001-ktkrau-authenticationsy-qpwu2h647bg.ws-us62.gitpod.io/api/token', opts)
-			.then(resp => {
-				if (resp.status === 200) return resp.json();
-				else alert("There has been some error");
-			})
-			.then()
-			.catch(error => {
-				console.error("There was an error", error)
-			})
-	}
+		}) */
+
+	};
+	if (store.token && store.token != "" && store.token != undefined) navigate("/");
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			<div>
-				<input type="text" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)}></input>
-				<input type="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
-				<button onClick={handleClick} >Login</button>
+			{(store.token && store.token != "" && store.token != undefined) ? "You are logged in with this token" + store.token :
+				<div>
+					<input type="text" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)}></input>
+					<input type="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
+					<button onClick={handleClick} >Login</button>
+				</div>
+
+			}
 
 
-			</div>
-
-		</div>
+		</div >
 	);
 };
 /* {
